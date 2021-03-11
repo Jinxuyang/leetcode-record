8c214c5e2bcd0a86fa3b1260a5789899e9ccb72b
jinxuyang <jinxuyang3@gmail.com>
2021-03-11 21:45:11 +0800

    add leetcode350

---
+++ src/Leetcode350.java
@@ -0,0 +1,84 @@
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author Verge
+ * @Date 2020/11/8 15:06
+ * @Version 1.0
+ */
+public class Leetcode350 {
+    public static void main(String[] args) {
+        int[] nums1 = {4, 9, 5};
+        int[] nums2 = {9, 4, 9, 8, 4};
+        System.out.println(Arrays.toString(intersect(nums1, nums2)));
+    }
+
+    /**
+     * 执行用时：3 ms, 在所有 Java 提交中击败了70.92%的用户
+     * 内存消耗：38.9 MB, 在所有 Java 提交中击败了5.13%的用户
+     * 思路：
+     * 1. 统计第一个数组各个数组的出现次数，使用hashmap存储
+     * 2. 遍历第二个数组，如果出现hashmap中存在的则将该数存在数组中，并将该数的出现次数-1
+     * 3. 返回数组
+     */
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        int[] temp;
+        if(nums1.length > nums2.length) temp = new int[nums2.length];
+        else temp = new int[nums1.length];
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i : nums1) {
+            if (map.containsKey(i)) map.put(i,map.get(i)+1);
+            else map.put(i,1);
+        }
+        int cnt = 0;
+        for (int i : nums2) {
+            if (map.containsKey(i)) {
+                if (map.get(i) > 0){
+                    temp[cnt] = i;
+                    map.put(i,map.get(i)-1);
+                    cnt ++;
+                }
+            }
+        }
+        int[] r = new int[cnt];
+        for(int i = 0;i < cnt;i++) r[i] = temp[i];
+        return r;
+    }
+    /**
+     * 空间复杂度优化：
+     * 1. 可以遍历较短的数组把数组信息存在hashmap中
+     * 时间复杂度优化：
+     * 1. 在第二次遍历时当数字的出现个数小于或等于1时将其去除，以减少下次搜索map的时间(貌似没用hashmap的操作的时间复杂度是O(1))
+     * 其他优化：
+     * 1. 用getOrDefault方法代替
+     * if (map.containsKey(i)) map.put(i,map.get(i)+1);
+     * else map.put(i,1);
+     * 2.使用Arrays.copyOfRange(temp,0,cnt)代替那一大串
+     *
+     * 执行用时：3 ms, 在所有 Java 提交中击败了70.92%的用户
+     * 内存消耗：38.3 MB, 在所有 Java 提交中击败了97.82%的用户
+     */
+
+    public static int[] intersect1(int[] nums1, int[] nums2) {
+        if(nums1.length > nums2.length) return intersect1(nums2,nums1);
+        int[] temp = new int[nums1.length];
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i : nums1) {
+           int count = map.getOrDefault(i,0) + 1;
+           map.put(i,count);
+        }
+        int cnt = 0;
+        for (int i : nums2) {
+            int count = map.getOrDefault(i,0);
+            if (count > 0){
+                temp[cnt] = i;
+                cnt++;
+                if (count - 1 <= 0) map.remove(i);
+                else map.put(i,count-1);
+            }
+        }
+        return Arrays.copyOfRange(temp,0,cnt);
+    }
+}
