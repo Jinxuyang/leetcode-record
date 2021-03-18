aec133c18a7e84c7e1d3e598b7823507350ec44a
jinxuyang <jinxuyang3@gmail.com>
2021-03-18 19:33:27 +0800

    add Leetcode2

--- src/linkedList/Leetcode2.java
+++ src/linkedList/Leetcode2.java
@@ -6,16 +6,12 @@ package linkedList;
  * @Version 1.0
  */
 public class Leetcode2 {
-    private class ListNode {
+    private static class ListNode {
       int val;
       ListNode next;
       ListNode() {}
       ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-    }
-
-    public static void main(String[] args) {
-
     }
 
     /**
