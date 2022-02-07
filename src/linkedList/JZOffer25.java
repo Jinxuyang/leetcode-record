8f2d3452ba4b53f1f145c1ced1e10d5b515a31e3
Verge <jinxuyang@163.com>
2022-02-07 10:51:57 +0800

    update JZOffer25

--- src/linkedList/JZOffer25.java
+++ src/linkedList/JZOffer25.java
@@ -40,4 +40,31 @@ public class JZOffer25 {
 
         return head;
     }
+
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode head = new ListNode(0);
+        ListNode temp = head.next;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                temp = l1;
+                l1 = l1.next;
+            } else {
+                temp = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+
+        if (l1 != null) {
+            temp.next = l1;
+        } else{
+            temp.next = l2;
+        }
+
+        return head.next;
+    }
 }
