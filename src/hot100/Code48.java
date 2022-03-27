2abe7ce3564ba0a6ef33d7f2f0b10584489486bf
Verge <jinxuyang@163.com>
2022-03-27 17:27:52 +0800

    add 48 in hot100

---
+++ src/hot100/Code48.java
@@ -0,0 +1,21 @@
+package hot100;
+
+/**
+ * @Author Verge
+ * @Date 2022/3/27 16:27
+ * @Version 1.0
+ */
+public class Code48 {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < (n + 1) / 2; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+}
