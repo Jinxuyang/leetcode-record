38fed4d8b48f8b1758f69e24965bba34264db995
Verge <jinxuyang@163.com>
2022-03-29 14:53:02 +0800

    add 78 in hot100

---
+++ src/hot100/Code78.java
@@ -0,0 +1,34 @@
+package hot100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author Verge
+ * @Date 2022/3/29 10:50
+ * @Version 1.0
+ */
+public class Code78 {
+    public static void main(String[] args) {
+        Code78 code78 = new Code78();
+        int[] nums = {1};
+        System.out.println(code78.subsets(nums));
+    }
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> subsets(int[] nums) {
+        recur(nums, 0, new ArrayList<>());
+        return res;
+    }
+
+    public void recur(int[] nums, int index, List<Integer> list) {
+        if (index == nums.length) {
+            res.add(list);
+            return;
+        }
+
+        list.add(nums[index]);
+        recur(nums, index + 1, new ArrayList<>(list));
+        list.remove(list.size() - 1);
+        recur(nums, index + 1, new ArrayList<>(list));
+    }
+}
